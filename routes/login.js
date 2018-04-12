1634fd839f73fe7d6c0806a5c4acb86b6e3ced25
Mohamed Salah <mohamed.salah@badrit.com>
2018-04-12 16:16:48 +0200

    add login route

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET login page. */
 router.get('/', function(req, res, next) {
-	res.sendfile('./public/index.html');
+	res.sendfile('./public/login.html');
 });
 
 module.exports = router;
