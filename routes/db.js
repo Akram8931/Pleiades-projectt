8e2a820403e87296332f5371c023112e4019e376
Mohamed Salah <mohamed.salah@badrit.com>
2018-04-15 20:07:09 +0200

    add us map api

---
+++ routes/db.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "root",
+	database: "presence"
+});
+module.exports.getUsMap = function(req, res, next) {
+	connection.connect(function(err) {
+		if (err) throw err;
+		var data = [];
+		connection.query("SELECT state_resides_in, COUNT(employee_id_number) AS number_of_employees FROM od GROUP BY state_resides_in", function (err, result, fields) {
+			if (err) throw err;
+			for (var i = 0; i < result.length; i++) {
+				data.push([result[i].state_resides_in, result[i].number_of_employees])
+			}
+			res.send(data);
+		});
+	});
+};
