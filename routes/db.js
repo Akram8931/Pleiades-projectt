65d2b78d014a6e845a92657dc86146a1b7b18672
Mohamed Salah <mohamed.salah@badrit.com>
2018-04-16 15:36:50 +0200

    add getCrossOrgCapability and getFunctionalCapability API

--- routes/db.js
+++ routes/db.js
@@ -5,6 +5,7 @@ var connection = mysql.createConnection({
 	password: "root",
 	database: "presence"
 });
+
 module.exports.getUsMap = function(req, res, next) {
 	var data = [];
 	connection.query("SELECT state_resides_in, COUNT(employee_id_number) AS number_of_employees FROM od GROUP BY state_resides_in", function (err, result, fields) {
@@ -15,3 +16,29 @@ module.exports.getUsMap = function(req, res, next) {
 		res.send(data);
 	});
 };
+
+module.exports.getFunctionalCapability = function(req, res, next) {
+	var data = [];
+	connection.query("SELECT home_location, functional_capability, COUNT(employee_id_number) AS number_of_employees FROM presence.od GROUP BY home_location, functional_capability", function (err, result, fields) {
+		if (err) throw err;
+		for (var i = 0; i < result.length; i++) {
+			data.push([result[i].home_location, result[i].functional_capability, result[i].number_of_employees])
+		}
+		res.send(data);
+	});
+};
+
+module.exports.getCrossOrgCapability = function(req, res, next) {
+	var data = [];
+	connection.query("SELECT home_location, cross_org_capability, COUNT(employee_id_number) AS number_of_employees FROM presence.od GROUP BY home_location, cross_org_capability", function (err, result, fields) {
+		if (err) throw err;
+		for (var i = 0; i < result.length; i++) {
+			data.push([result[i].home_location, result[i].cross_org_capability, result[i].number_of_employees])
+		}
+		res.send(data);
+	});
+};
+
+
+
+
