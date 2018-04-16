3be9154b60d680ff6ce25d78ed008327065c0873
aya.adel <aya.adel@badrit.com>
2018-04-16 15:33:52 +0200

    Add draw chart method

--- frontend/src/components/ChartSankey.vue
+++ frontend/src/components/ChartSankey.vue
@@ -1,66 +1,82 @@
 <template>
   <div class="sankey-chart">
+  
     <div id="sankey_basic"></div>
+    
   </div>
 </template>
 
 <script>
-export default {
-  name: "ChartSankey",
-  props: [
-    {
-      "digramData": {
-        required: true,
-        type: Array
+  export default {
+    name: "ChartSankey",
+    computed: {
+      changeData() {
+        this.drawSankeyChart();
+        return this.$store.state.selectedNode;
       }
-    }
-  ],
-  data() {
-    return {
-      x: this.$store.state.sankeyData
-    };
-  },
-  mounted() {
-    // google.charts.load('current', {'packages':['sankey']});
+  
+    },
 
-    google.setOnLoadCallback(drawChart);
+    methods: {
+  
+      drawSankeyChart() {
+        const self = this;
+        google.setOnLoadCallback(drawChart);
+  
+  
+        function drawChart() {
+        
+          
+          let data = new google.visualization.DataTable();
+  
+          data.addColumn("string", "From");
+          data.addColumn("string", "To");
+          data.addColumn("number", "Weight");
+  
+          data.addRows(self.$store.state.selectedNode);
+  
+          // Sets chart options.
+          let options = {
+            width: 600,
+            sankey: {
+              node: {
+                interactivity: true
+              }
+            }
+          };
+  
+          // Instantiates and draws our chart, passing in some options.
+          let chart = new google.visualization.Sankey(
+            document.getElementById("sankey_basic")
+          );
+  
+          chart.draw(data, options);
+  
+          window.google.visualization.events.addListener(chart, "select", () => {
+  
+            let sel = chart.getSelection();
+  
+            if (sel.length) {
+  
+              self.$store.commit('getSelectedNode', sel[0].name)
+              chart.clearChart();
+              drawChart();
 
-    function drawChart() {
-      console.log("x" + typeof this.x);
-
-      const data = new google.visualization.DataTable();
-      data.addColumn("string", "From");
-      data.addColumn("string", "To");
-      data.addColumn("number", "Weight");
-      if (this.x) data.addRows(this.x);
-
-      // Sets chart options.
-      const options = {
-        width: 600,
-        sankey: {
-          node: {
-            interactivity: true
-          }
+            }
+          });
         }
-      };
-
-      // Instantiates and draws our chart, passing in some options.
-      const chart = new google.visualization.Sankey(
-        document.getElementById("sankey_basic")
-      );
-      chart.draw(data, options);
-      window.google.visualization.events.addListener(chart, "select", () => {
-        const sel = chart.getSelection();
-        if (sel.length) {
-          console.log(sel);
-          // alert(`You selected node "${sel[0].name}"`);
-        }
-      });
+      }
+  
+    },
+    created() {
+  
+      this.$store.dispatch('initChart');
+      this.drawSankeyChart();
+  
     }
-  }
-};
+  };
 </script>
 
 <style scoped>
-
+  
 </style>
