29da92b2c9c15361a7779b46db6c2bba3e2f7fd5
Mohamed Salah <mohamed.salah@badrit.com>
2018-04-16 13:11:55 +0200

    fix db connection issue

--- routes/db.js
+++ routes/db.js
@@ -6,15 +6,12 @@ var connection = mysql.createConnection({
 	database: "presence"
 });
 module.exports.getUsMap = function(req, res, next) {
-	connection.connect(function(err) {
+	var data = [];
+	connection.query("SELECT state_resides_in, COUNT(employee_id_number) AS number_of_employees FROM od GROUP BY state_resides_in", function (err, result, fields) {
 		if (err) throw err;
-		var data = [];
-		connection.query("SELECT state_resides_in, COUNT(employee_id_number) AS number_of_employees FROM od GROUP BY state_resides_in", function (err, result, fields) {
-			if (err) throw err;
-			for (var i = 0; i < result.length; i++) {
-				data.push([result[i].state_resides_in, result[i].number_of_employees])
-			}
-			res.send(data);
-		});
+		for (var i = 0; i < result.length; i++) {
+			data.push([result[i].state_resides_in, result[i].number_of_employees])
+		}
+		res.send(data);
 	});
 };
