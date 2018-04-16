ed9b4408290e6e95dae950ea8f7e5c86557c09e0
aya.adel <aya.adel@badrit.com>
2018-04-16 16:56:58 +0200

    add chart draw

--- frontend/src/components/ChartSankey.vue
+++ frontend/src/components/ChartSankey.vue
@@ -2,81 +2,70 @@
   <div class="sankey-chart">
   
     <div id="sankey_basic"></div>
-    
+  {{changeData}}
   </div>
 </template>
 
 <script>
-  export default {
-    name: "ChartSankey",
-    computed: {
-      changeData() {
-        this.drawSankeyChart();
-        return this.$store.state.selectedNode;
-      }
-  
-    },
-
-    methods: {
-  
-      drawSankeyChart() {
-        const self = this;
-        google.setOnLoadCallback(drawChart);
-  
-  
-        function drawChart() {
-        
-          
-          let data = new google.visualization.DataTable();
-  
-          data.addColumn("string", "From");
-          data.addColumn("string", "To");
-          data.addColumn("number", "Weight");
-  
-          data.addRows(self.$store.state.selectedNode);
-  
-          // Sets chart options.
-          let options = {
-            width: 600,
-            sankey: {
-              node: {
-                interactivity: true
-              }
-            }
-          };
-  
-          // Instantiates and draws our chart, passing in some options.
-          let chart = new google.visualization.Sankey(
-            document.getElementById("sankey_basic")
-          );
-  
-          chart.draw(data, options);
-  
-          window.google.visualization.events.addListener(chart, "select", () => {
-  
-            let sel = chart.getSelection();
-  
-            if (sel.length) {
-  
-              self.$store.commit('getSelectedNode', sel[0].name)
-              chart.clearChart();
-              drawChart();
-
-            }
-          });
-        }
-      }
-  
-    },
-    created() {
-  
-      this.$store.dispatch('initChart');
+export default {
+  name: "ChartSankey",
+  props:['dataChart'],
+  computed: {
+    changeData() {
       this.drawSankeyChart();
-  
+      return this.$store.state.selectedNode;
     }
-  };
+  },
+  methods: {
+    drawSankeyChart() {
+      const self = this;
+      google.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        let data = new google.visualization.DataTable();
+
+        data.addColumn("string", "From");
+        data.addColumn("string", "To");
+        data.addColumn("number", "Weight");
+
+        data.addRows(self.$store.state.selectedNode);
+
+        // Sets chart options.
+        let options = {
+          width: 600,
+          sankey: {
+            node: {
+              interactivity: true
+            }
+          }
+        };
+
+        // Instantiates and draws our chart, passing in some options.
+        let chart = new google.visualization.Sankey(
+          document.getElementById("sankey_basic")
+        );
+
+        chart.draw(data, options);
+
+        window.google.visualization.events.addListener(chart, "select", () => {
+          let sel = chart.getSelection();
+
+          if (sel.length) {
+            self.$store.commit("getSelectedNode", sel[0].name);
+            chart.clearChart();
+            drawChart();
+          }
+        });
+      }
+    }
+  },
+  mounted() {
+    this.$store.dispatch('initChart');    
+    this.drawSankeyChart();
+  }
+};
 </script>
 
 <style scoped>
-  
+
 </style>
