b2df8f1ad24896e4cd3e5e230d9d9b8632628e55
aya.adel <aya.adel@badrit.com>
2018-04-16 15:28:18 +0200

    Add Working Store File

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,11 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
+const store = new Vuex.Store({
   state: {
     sankeyData: [['A', 'X', 5], ['A', 'Y', 7], ['A', 'Z', 6], ['B', 'X', 2], ['B', 'Y', 9], ['B', 'Z', 4]],
   },
 });
+
+export { store as default };
+
