045a201a503c4ce58ca94c87d62b24a1b6d8ad5f
hadeer mohamed <hadeer.mohamed@badrit.com>
2018-04-16 16:10:17 +0200

    pie chart

--- frontend/src/components/Demographic.vue
+++ frontend/src/components/Demographic.vue
@@ -1,7 +1,22 @@
 <template>
 <div>
 <div id="visualization" style="margin: 1em"> </div>
-
+  <div>
+      <b-modal v-model="$store.state.modalShow" title="State Pie Chart">
+      <div class="d-block text-center">
+      <div class="col-xs-4">
+        <vue-chart chart-type="PieChart" :columns="$store.state.PieColumns" :rows="$store.state.PieGenderRows" :options="$store.state.PieGenderOptions"></vue-chart>
+      </div>
+        <div class="col-xs-4">
+<vue-chart chart-type="PieChart" :columns="$store.state.PieColumns" :rows="$store.state.PieAgeRows" :options="$store.state.PieAgeOptions"></vue-chart>
+      </div>
+<div class="col-xs-4">
+<vue-chart chart-type="PieChart" :columns="$store.state.PieColumns" :rows="$store.state.PieRaceRows" :options="$store.state.PieRaceOptions"></vue-chart>
+      </div>
+      </div>
+      <b-btn class="mt-3" variant="outline-danger" block>Close Me</b-btn>
+    </b-modal>
+  </div>
 
 </div>
 </template>
@@ -16,6 +31,9 @@ export default {
   created() {
     this.$store.dispatch('loadUSAMap');
   },
+    methods: {
+
+  }
 
 };
 </script>
