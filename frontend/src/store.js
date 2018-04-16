284bcb5a602d589ee3404043066e631c9be098ba
aya.adel <aya.adel@badrit.com>
2018-04-16 16:56:11 +0200

    Get data from api

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-// import axios from 'axios';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
@@ -11,7 +11,8 @@ const store = new Vuex.Store({
   },
   mutations: {
     setChartData(state, sankeyData) {
-      state.selectedNode = sankeyData;
+      state.sankeyData = sankeyData;
+      state.selectedNode = state.sankeyData;
     },
     getSelectedNode(state, selectedNodeName) {
       state.selectedNode = _.filter(state.sankeyData, (o) => {
@@ -25,7 +26,17 @@ const store = new Vuex.Store({
   },
   actions: {
     initChart({ commit }) {
-      commit('setChartData', [['A', 'X', 5], ['A', 'Y', 7], ['A', 'Z', 6], ['B', 'X', 2], ['B', 'Y', 9], ['B', 'Z', 4]]);
+      axios.get('http://red-alphar.com/functional_capability')
+        .then((response) => {
+          commit('setChartData', response.data);
+          console.log(response.data);
+          console.log("i got it")
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      // commit('setChartData', [['A', 'X', 5], ['A', 'Y', 7], ['A', 'Z', 6], ['B', 'X', 2], ['B', 'Y', 9], ['B', 'Z', 4]]);
     },
   },
 });
