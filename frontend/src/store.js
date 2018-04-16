e360745be2a9fa5834f80a3fa4055b23b83948af
aya.adel <aya.adel@badrit.com>
2018-04-16 15:32:19 +0200

    change chart data with selected Node data

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,6 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-export const store = new Vuex.Store({
 const store = new Vuex.Store({
   state: {
     sankeyData: [['A', 'X', 5], ['A', 'Y', 7], ['A', 'Z', 6], ['B', 'X', 2], ['B', 'Y', 9], ['B', 'Z', 4]],
@@ -13,6 +12,15 @@ const store = new Vuex.Store({
   mutations: {
     setChartData(state, sankeyData) {
       state.selectedNode = sankeyData;
+    },
+    getSelectedNode(state, selectedNodeName) {
+      state.selectedNode = _.filter(state.sankeyData, (o) => {
+
+        if (o.indexOf(selectedNodeName) > -1) {
+          return o;
+        }
+
+      });
     },
   },
   actions: {
