58f96d0d55d12ee6e6c37c0b60516a64b153fdda
aya.adel <aya.adel@badrit.com>
2018-04-17 13:02:18 +0200

    add waiting class for loadin chart

--- frontend/src/components/ChartSankey.vue
+++ frontend/src/components/ChartSankey.vue
@@ -1,71 +1,108 @@
 <template>
   <div class="sankey-chart">
+    <div class="container">
   
-    <div id="sankey_basic"></div>
-  {{changeData}}
+      <div class="loading" v-if="!loading">
+        Loading...
+      </div>
+  
+      <div v-else>
+        <div id="sankey_basic"></div>
+        {{changeData}}
+      </div>
+  
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "ChartSankey",
-  props:['dataChart'],
-  computed: {
-    changeData() {
-      this.drawSankeyChart();
-      return this.$store.state.selectedNode;
-    }
-  },
-  methods: {
-    drawSankeyChart() {
-      const self = this;
-      google.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-        let data = new google.visualization.DataTable();
-
-        data.addColumn("string", "From");
-        data.addColumn("string", "To");
-        data.addColumn("number", "Weight");
-
-        data.addRows(self.$store.state.selectedNode);
-
-        // Sets chart options.
-        let options = {
-          width: 600,
-          sankey: {
-            node: {
-              interactivity: true
-            }
-          }
-        };
-
-        // Instantiates and draws our chart, passing in some options.
-        let chart = new google.visualization.Sankey(
-          document.getElementById("sankey_basic")
-        );
-
-        chart.draw(data, options);
-
-        window.google.visualization.events.addListener(chart, "select", () => {
-          let sel = chart.getSelection();
-
-          if (sel.length) {
-            self.$store.commit("getSelectedNode", sel[0].name);
-            chart.clearChart();
-            drawChart();
-          }
-        });
+  export default {
+    name: "ChartSankey",
+    props: ["dataChart"],
+    data() {
+      return {
+        loading: false,
       }
+    },
+    watch: {
+      // '$route': 'drawSankeyChart'
+      '$route' (to, from) {
+        this.$store.commit('activeChartData', to.params.name)
+      },
+      dataLoaded(){
+        this.$store.commit('activeChartData', this.$route.params.name)
+        this.drawSankeyChart();
+        
+      }
+    },
+    computed: {
+      changeData() {
+        return this.$store.state.selectedNode;
+      },
+      dataLoaded() {
+        return this.$store.state.functionalChartData;
+      }
+    },
+    methods: {
+      drawSankeyChart() {
+        
+        this.loading = true;
+  
+        const self = this;
+
+        google.setOnLoadCallback(drawChart);
+        console.log(google)
+
+        console.log("lets go")
+  
+        function drawChart() {
+          console.log("drawwwwwwwwwwww me opa")
+          
+          let data = new google.visualization.DataTable();
+  
+          data.addColumn("string", "From");
+          data.addColumn("string", "To");
+          data.addColumn("number", "Weight");
+  
+          data.addRows(self.$store.state.selectedNode);
+  
+          // Sets chart options.
+          let options = {
+            width: 1200,
+            sankey: {
+              node: {
+                interactivity: true,
+                width: 10
+              }
+            }
+          };
+  
+          // Instantiates and draws our chart, passing in some options.
+          let chart = new google.visualization.Sankey(
+            document.getElementById("sankey_basic")
+          );
+  
+          chart.draw(data, options);
+          window.google.visualization.events.addListener(chart, "select", () => {
+            let sel = chart.getSelection();
+  
+            if (sel.length) {
+              self.$store.commit("getSelectedNode", sel[0].name);
+              chart.clearChart();
+              drawChart();
+            }
+          });
+        }
+      }
+    },
+    created() {
+        
+      this.$store.dispatch("initChart");
+  
     }
-  },
-  mounted() {
-    this.$store.dispatch('initChart');    
-    this.drawSankeyChart();
-  }
-};
+  };
 </script>
 
 <style scoped>
-
+  
 </style>
