d0cd5d8d9fcb029bc569db55dffed3eb08979dd6
Mohamed Salah <mohamed.salah@badrit.com>
2018-04-18 18:58:58 +0200

    add login route

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -100,6 +100,9 @@ const mutations = {
     state.status = 'success';
     state.token = payload.token;
     state.expiryDate = payload.expiryDate;
+    if (Date.now() < state.expiryDate) {
+      state.isExpired = false;
+    }
   },
   AUTH_ERROR(state) {
     state.status = 'error';
@@ -147,7 +150,7 @@ const actions = {
   AUTH_REQUEST({ commit }, user) {
     const NPromise = new Promise((resolve, reject) => {
       commit('AUTH_REQUEST');
-      axios({ url: 'auth', data: user, method: 'POST' })
+      axios({ url: 'http://localhost:3000/login', data: user, method: 'POST' })
         .then((resp) => {
           const token = resp.data.token;
           const expiryDate = resp.data.expiryDate;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,55 @@
-var express = require('express');
-var router = express.Router();
+var db = require('./db');
+var jwt = require('jsonwebtoken');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-module.exports = router;
+exports.login = function(req, res, next) {
+	var username=req.body.username;
+ 	var password=req.body.password;
+ 	db.getUser(username, password, function(results){
+ 		if(results.length){
+ 			var data = JSON.stringify(results);
+ 			var secret = 'TOPSECRETTTTT';
+			var now = Math.floor(Date.now() / 1000),
+				iat = (now - 10),
+				expiresIn = 3600 * 3600 * 3600,
+				expr = (now + expiresIn),
+				notBefore = (now - 10),
+				jwtId = Math.random().toString(36).substring(7);
+			var payload = {
+				iat: iat,
+				jwtid : jwtId,
+				audience : 'TEST',
+				data : data
+			};	
+			jwt.sign(payload, secret, { algorithm: 'HS256', expiresIn : expiresIn}, function(err, token) {
+					
+				if(err){
+					res.status(500).json(err);
+					res.end();
+				}
+				 else{
+				if(token != false){
+					res.header();
+					res.json({
+  						"token": token,
+						"expiryDate": Date.now() + expiresIn
+  					});
+					res.send();
+				}
+				else{
+					res.send("Could not create token");
+					res.end();
+				}
+				
+				 }
+			});
+ 		}else{
+ 			var output = {
+ 				error: {
+ 					message: "not a user",
+ 				}
+ 			};
+ 			res.status(500).json(output);
+ 			res.end();
+ 		}
+ 	})
+};
